79ea4ee480339d7f0a21c1f07969ec659bfc3dfb
rudiansyahirun <rudd.rocker@gmail.com>
2022-02-26 02:08:08 +0700

    update function chapter - builtin functions

---
+++ src/course/LearningJava/FunctionsInJava/BuiltinFunctions.java
@@ -0,0 +1,12 @@
+package course.LearningJava.FunctionsInJava;
+
+public class BuiltinFunctions {
+    public static void main(String[] args) {
+        double result = Math.pow(2, 5);
+        System.out.println(result);
+
+        double aSin = Math.asin(.3);
+        System.out.println(aSin);
+
+    }
+}
