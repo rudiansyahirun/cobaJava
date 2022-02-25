dc3ab019db53adda19b182d1ee7be24b8844ec86
rudiansyahirun <rudd.rocker@gmail.com>
2022-02-26 01:18:00 +0700

    add function chapter

---
+++ src/course/LearningJava/FuntionsInJava/DeveloperTeaTime.java
@@ -0,0 +1,26 @@
+package course.LearningJava.FuntionsInJava;
+
+import java.util.Scanner;
+
+public class DeveloperTeaTime {
+    public static void announceDeveloperTeaTime(){
+        System.out.println("Waiting developer tea time...");
+        System.out.println("Type a random word and press Enter to start developer tea time");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer te time!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to your new job");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Write code");
+        System.out.println("Review code");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Get promoted!");
+    }
+}
