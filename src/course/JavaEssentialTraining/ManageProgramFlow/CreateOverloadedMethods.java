68f838f516f82a8bd90dc0d6e0b6ad88afc4c80c
rudiansyahirun <rudd.rocker@gmail.com>
2022-02-26 16:08:00 +0700

    update CreateOverloadedMethods

--- src/course/JavaEssentialTraining/ManageProgramFlow/CreateOverloadedMethods.java
+++ src/course/JavaEssentialTraining/ManageProgramFlow/CreateOverloadedMethods.java
@@ -12,6 +12,12 @@ public class CreateOverloadedMethods {
         double result = addValues(i1, i2);
         System.out.println("The sum is " + result);
 
+        double resultOfFour = addValues(i1, i2, i1, i2);
+        System.out.println("The sum of 4 is " + resultOfFour);
+
+        double resultOfMultiple = addValues(i1, i2, i1, i2, i1, i2);
+        System.out.println("The sum of multiple is " + resultOfMultiple);
+
     }
 
     private static int getInput(Scanner sc, String prompt) {
@@ -21,5 +27,17 @@ public class CreateOverloadedMethods {
 
     private static double addValues(int i1, int i2) {
         return i1 + i2;
+    }
+
+    private static double addValues(int i1, int i2, int i3, int i4) {
+        return i1 + i2 + i3 + i4;
+    }
+
+    private static double addValues(int... values) {
+        int result = 0;
+        for (var value : values) {
+            result += value;
+        }
+        return result;
     }
 }
