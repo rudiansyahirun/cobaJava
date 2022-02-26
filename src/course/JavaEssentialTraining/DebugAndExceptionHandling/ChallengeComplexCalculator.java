386083238da624db7e8733de906838a594bf9ebe
rudiansyahirun <rudd.rocker@gmail.com>
2022-02-26 23:44:51 +0700

    add ChallengeComplexCalculator

---
+++ src/course/JavaEssentialTraining/DebugAndExceptionHandling/ChallengeComplexCalculator.java
@@ -0,0 +1,49 @@
+package course.JavaEssentialTraining.DebugAndExceptionHandling;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ChallengeComplexCalculator {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+
+        double d1, d2;
+
+        try {
+            System.out.println("Enter a numeric value: ");
+            d1 = sc.nextDouble();
+            sc.nextLine();
+
+            System.out.println("Enter a numeric value: ");
+            d2 = sc.nextDouble();
+            sc.nextLine();
+
+        } catch (InputMismatchException e) {
+            System.out.println("Couldn't format as a number");
+            return;
+        }
+
+        System.out.println("Select an operation (+ - * /): ");
+        var operation = sc.nextLine();
+
+        double result;
+        switch (operation) {
+            case "+":
+                result = d1 + d2;
+                break;
+            case "-":
+                result = d1 - d2;
+                break;
+            case "*":
+                result = d1 * d2;
+                break;
+            case "/":
+                result = d1 / d2;
+                break;
+            default:
+                System.out.println("You didn't choose a valid operation");
+                return;
+        }
+        System.out.println("The answer is " +result);
+    }
+}
