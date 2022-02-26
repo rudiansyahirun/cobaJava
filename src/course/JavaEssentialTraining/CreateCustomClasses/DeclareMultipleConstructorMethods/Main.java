b038221e68417de7d4522ba626924e991943eeaf
rudiansyahirun <rudd.rocker@gmail.com>
2022-02-27 00:51:13 +0700

    add DeclareMultipleConstructorMethods

---
+++ src/course/JavaEssentialTraining/CreateCustomClasses/DeclareMultipleConstructorMethods/Main.java
@@ -0,0 +1,22 @@
+package course.JavaEssentialTraining.CreateCustomClasses.DeclareMultipleConstructorMethods;
+
+import course.JavaEssentialTraining.CreateCustomClasses.DeclareMultipleConstructorMethods.model.ClothingItem;
+
+import java.text.NumberFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        var item = new ClothingItem("Shirt", "M", 19.99,3);
+//        item.setType("Shirt");
+//        item.setSize("M");
+//        item.setPrice(19.99);
+//        item.setQuantity(3);
+
+        var totalPrice = item.getPrice() * item.getQuantity();
+        var formatter = NumberFormat.getCurrencyInstance();
+        var output = String.format("Your %s order will cost %s",
+                                            item.getType(),
+                                            formatter.format(totalPrice));
+        System.out.println(output);
+    }
+}
