140f2a971a184f8d5bfdb748fafd2d0727d39f28
rudiansyahirun <rudd.rocker@gmail.com>
2022-02-26 14:58:18 +0700

    add EvaluateConditionsWithIfElse

---
+++ src/course/JavaEssentialTraining/ManageProgramFlow/EvaluateConditionsWithIfElse.java
@@ -0,0 +1,23 @@
+package course.JavaEssentialTraining.ManageProgramFlow;
+
+import java.util.Scanner;
+
+public class EvaluateConditionsWithIfElse {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.print("Enter a month number (1-12): ");
+        var monthNumber = scanner.nextInt();
+
+        String message;
+        if (monthNumber < 1 || monthNumber > 12) {
+            message = "That isn't a valid month!";
+        } else if (monthNumber <= 3) {
+            message = "That's in Quarter 1!";
+        } else if (monthNumber <= 6) {
+            message = "That's in Quarter 2!";
+        } else {
+            message = "That isn't in the first half of the year";
+        }
+        System.out.println(message);
+    }
+}
