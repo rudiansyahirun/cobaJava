aba892ae57a11f2f4ccd82b5a41fba45eeec7fe2
rudiansyahirun <rudd.rocker@gmail.com>
2022-02-28 04:55:50 +0700

    add Classes

---
+++ src/course/LearningJavaApplication/JavaBasicsReview/Classes/Main.java
@@ -0,0 +1,11 @@
+package course.LearningJavaApplication.JavaBasicsReview.Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        OtherClass oc = new OtherClass();
+        oc.myMethod();
+
+        System.out.println(oc.myInt);
+
+    }
+}
---
+++ src/course/LearningJavaApplication/JavaBasicsReview/Classes/OtherClass.java
@@ -0,0 +1,12 @@
+package course.LearningJavaApplication.JavaBasicsReview.Classes;
+
+public class OtherClass {
+    OtherClass(){
+        System.out.println("a constuctor from another class");
+    }
+
+    void myMethod() {
+        System.out.println("a method from another class");
+    }
+    int myInt = 5;
+}
